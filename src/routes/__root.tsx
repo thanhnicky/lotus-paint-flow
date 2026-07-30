9e2e6ffd6892a21e6fc3b80a1f11de16c36b45d2
thanhnicky <aicathanh@gmail.com>
2026-07-30 10:35:53 +0700

    Update Google Tag Manager to GTM-T5PGMZ8D

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -222,19 +222,28 @@ function RootShell({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-13XJT8M29C" />
         <script
           dangerouslySetInnerHTML={{
-            __html: `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-13XJT8M29C');
-gtag('config', 'AW-16701011893');`,
+            __html: `<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-T5PGMZ8D');</script>
+<!-- End Google Tag Manager -->`,
           }}
         />
         <HeadContent />
       </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T5PGMZ8D"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
         <Scripts />
       </body>
